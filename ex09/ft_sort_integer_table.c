251d23c78708165806fc1d565c7b362bfb09a520
Deshi Mokgokong <demokgok@c5r5s5.wethinkcode.co.za>
2020-07-15 09:11:19 +0200

    check pls

---
+++ ex09/ft_sort_integer_table.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_integer_table.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: demokgok <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/15 08:59:57 by demokgok          #+#    #+#             */
+/*   Updated: 2020/07/15 09:07:21 by demokgok         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include<unistd.h>
+
+int *ft_sort_integer_table(int *tab, int size)
+{
+	int count;
+	int = i;
+	int = tmp;
+
+	count = 0;
+	while (count < size)
+	{
+		i = 0;
+		while (i < (size - 1))
+		{
+			if(tab[i] > tab[i+1])
+				tab[i+1] = tmp
+		}
+		1++;
+	}
+	count ++;
+}
+return (tab);}
